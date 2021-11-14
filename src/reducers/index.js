06e731c43e8ee572005850e5f96b9f3d01ba07dc
Victor oc <ochando3d@gmail.com>
2021-11-14 14:50:59 +0000

    Initial setup. Basic action and reducer creation.

---
+++ src/reducers/index.js
@@ -0,0 +1,15 @@
+const songsReducer = () => {
+    return [
+        { title: "title 1", duration: "2:34" },
+        { title: "title 2", duration: "3:15" },
+        { title: "title 3", duration: "4:21" },
+        { title: "title 4", duration: "2:05" },
+    ];
+};
+
+const selectedSongReducer = (selectedSong = null, action) => {
+    if (action.type === "SONG_SELECTED") {
+        return action.payload;
+    }
+    return selectedSong;
+};
