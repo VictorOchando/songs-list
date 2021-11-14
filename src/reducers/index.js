8edc3293918108c8c1d5b5c7d40dad6afa38966a
Victor oc <ochando3d@gmail.com>
2021-11-14 14:55:17 +0000

    combine reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,3 +1,5 @@
+import { combineReducers } from "redux";
+
 const songsReducer = () => {
     return [
         { title: "title 1", duration: "2:34" },
@@ -13,3 +15,8 @@ const selectedSongReducer = (selectedSong = null, action) => {
     }
     return selectedSong;
 };
+
+export default combineReducers({
+    songs: songsReducer,
+    selectedSong: selectedSongReducer,
+});
